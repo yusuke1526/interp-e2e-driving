f6cdde7a5030a64fa135f8e78cef80a91126229e
yusuke1526 <yusukemail1526@gmail.com>
2023-05-15 18:21:05 +0900

    implemented vision model

--- interp_e2e_driving/networks/world_model.py
+++ interp_e2e_driving/networks/world_model.py
@@ -21,13 +21,14 @@ class VisionModel(tf.Module):
   """VAE
   """
 
-  def __init__(self, input_names, reconstruct_names, obs_size=64, base_depth=32, latent_size=64, decoder_stddev=np.sqrt(0.1, dtype=np.float32), name=None):
+  def __init__(self, input_names, reconstruct_names, obs_size, latent_size, r_loss_factor=0.1, base_depth=32, decoder_stddev=np.sqrt(0.1, dtype=np.float32), name=None):
     super(VisionModel, self).__init__(name=name)
     self.input_names = input_names
     self.reconstruct_names = reconstruct_names
     self.obs_size = obs_size
     self.base_depth = base_depth
     self.latent_size = latent_size
+    self.r_loss_factor= r_loss_factor
 
     # Create encoders q(f_t|x_t)
     self.encoders = {}
@@ -52,21 +53,63 @@ class VisionModel(tf.Module):
         self.decoders[name] = Decoder256(base_depth, scale=decoder_stddev)
       else:
         raise NotImplementedError
+      
+    # self.vae_z_means = {name: tf.keras.layers.Dense(self.latent_size) for name in self.input_names}
+    # self.vae_z_log_vars = {name: tf.keras.layers.Dense(self.latent_size) for name in self.input_names}
+
+    self.vae_z_mean = tf.keras.layers.Dense(self.latent_size)
+    self.vae_z_log_var = tf.keras.layers.Dense(self.latent_size)
 
   def __call__(self, images):
+    z_mean, z_log_var, z = self.encode(images)
+    reconstructions = self.decode(z)
+
+    return z, reconstructions
+  
+  def get_z_dist(self, images):
     features = {}
     for name, encoder in self.encoders.items():
         features[name] = encoder(images[name])
-    reconstructs = {}
+    feature = tf.concat(list(features.values()), axis=-1)
+    z_mean = self.vae_z_mean(feature)
+    z_log_var = self.vae_z_log_var(feature)
+    return z_mean, z_log_var
+
+  def encode(self, images):
+    z_mean, z_log_var = self.get_z_dist(images)
+    z = self.sample_z(z_mean, z_log_var)
+    return z_mean, z_log_var, z
+  
+  def decode(self, z):
+    reconstructions = {}
     for name, decoder in self.decoders.items():
-        reconstructs[name] = decoder(features[name]).mean()
-    return features, reconstructs
+        reconstructions[name] = decoder(z).mean()
+        print(f'{name}: {reconstructions[name].shape}')
+    return reconstructions
   
   def init_prior_distribution(self):
     return None #TODO
   
-  def sample_z(self, mean, std):
-    return self.prior(loc=mean, scale_diag=std).sample()
+  def sample_z(self, mu, log_var):
+    epsilon = tf.keras.backend.random_normal(shape=mu.shape, mean=0., stddev=1.)
+    return mu + tf.math.exp(log_var / 2) * epsilon
+  
+  def compute_loss(self, images):
+    z_mean, z_log_var, z = self.encode(images)
+    reconstruction = self.decode(z)
+    reconstruction_losses = []
+    for name in self.reconstruct_names:
+      reconstruction_losses.append(tf.reduce_mean(
+          tf.square(images[name] - reconstruction[name]), axis = [1,2,3]
+      ))
+    reconstruction_loss = tf.stack(reconstruction_losses, axis=-1)
+    reconstruction_loss = tf.reduce_mean(reconstruction_loss, axis=-1)
+    reconstruction_loss *= self.r_loss_factor
+    kl_loss = 1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)
+    kl_loss = tf.reduce_sum(kl_loss, axis = 1)
+    kl_loss *= -0.5
+    total_loss = reconstruction_loss + kl_loss
+    return tf.reduce_mean(kl_loss), tf.reduce_mean(reconstruction_loss), tf.reduce_mean(total_loss)
 
 @gin.configurable
 class MemoryModel(tf.Module):
@@ -428,43 +471,46 @@ class WorldModel(tf.Module):
     return latent_samples, latent_dists
   
 if __name__ == '__main__':
+  batch_size = 16
 
   # Vision Model
-  # params = {
-  #   'base_depth': 32,
-  #   'input_names': ['rgb'],
-  #   'reconstruct_names': ['rgb'],
-  #   'obs_size': 128
-  # }
-  # rgb = tf.zeros([batch_size, params['obs_size'], params['obs_size'], 3], tf.float32)
-  # images = {'rgb': rgb}
-  # vision = VisionModel(**params)
-  # features, reconstructs = vision(images)
-  # print(features)
-  # print(reconstructs['rgb'].mean())
+  params = {
+    'base_depth': 32,
+    'input_names': ['rgb'],
+    'reconstruct_names': ['rgb', 'mask'],
+    'obs_size': 128,
+    'latent_size': 32,
+  }
+  rgb = tf.ones([batch_size, params['obs_size'], params['obs_size'], 3], tf.float32) * 10
+  mask = tf.ones([batch_size, params['obs_size'], params['obs_size'], 3], tf.float32) * 100
+  images = {'rgb': rgb, 'mask': mask}
+  vision = VisionModel(**params)
+  z, reconstructions = vision(images)
+  # print(z.shape)
+  # print(reconstructions['rgb'].shape)
+  loss = vision.compute_loss(images)
+  # print(loss)
 
   # Memory Model
-  batch_size = 128
+  # latent_size = 32
+  # action_size = 1
+  # timesteps = 1
+  # gaussian_mixtures = 5
 
-  latent_size = 32
-  action_size = 1
-  timesteps = 1
-  gaussian_mixtures = 5
+  # memory = MemoryModel(latent_size=latent_size, action_size=action_size, gaussian_mixtures=gaussian_mixtures)
+  # z = tf.random.normal([batch_size, timesteps, latent_size], 1, 1, tf.float32)
+  # a = tf.random.normal([batch_size, timesteps, action_size], 1, 1, tf.float32)
+  # h = tf.zeros([batch_size, latent_size + action_size + 1], tf.float32) # 1 is for reward
+  # c = tf.zeros([batch_size, latent_size + action_size + 1], tf.float32)
+  # prev_rew = tf.random.normal([batch_size, timesteps, 1], 1, 1, tf.float32)
 
-  memory = MemoryModel(latent_size=latent_size, action_size=action_size, gaussian_mixtures=gaussian_mixtures)
-  z = tf.random.normal([batch_size, timesteps, latent_size], 1, 1, tf.float32)
-  a = tf.random.normal([batch_size, timesteps, action_size], 1, 1, tf.float32)
-  h = tf.zeros([batch_size, latent_size + action_size + 1], tf.float32) # 1 is for reward
-  c = tf.zeros([batch_size, latent_size + action_size + 1], tf.float32)
-  prev_rew = tf.random.normal([batch_size, timesteps, 1], 1, 1, tf.float32)
-
-  print(h.shape)
-  print(z.shape)
-  print(a.shape)
-  print(memory)
-  # input_z, input_action, state_input_h, state_input_c
-  y_pred = memory.pred(z, a, prev_rew, h, c)
-  (log_pi, mu, log_sigma), rew_pred = y_pred
-  print((log_pi.shape, mu.shape, log_sigma.shape), rew_pred.shape)
-  y_true = tf.random.normal([batch_size, timesteps, latent_size + 1], 1, 1, tf.float32)
-  memory.compute_loss(y_pred, y_true)
+  # print(h.shape)
+  # print(z.shape)
+  # print(a.shape)
+  # print(memory)
+  # # input_z, input_action, state_input_h, state_input_c
+  # y_pred = memory.pred(z, a, prev_rew, h, c)
+  # (log_pi, mu, log_sigma), rew_pred = y_pred
+  # print((log_pi.shape, mu.shape, log_sigma.shape), rew_pred.shape)
+  # y_true = tf.random.normal([batch_size, timesteps, latent_size + 1], 1, 1, tf.float32)
+  # memory.compute_loss(y_pred, y_true)
